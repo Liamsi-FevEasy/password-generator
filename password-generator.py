e97484e763f251e318523b2a1980a11a3e18af1f
Liamsi-FevEasy <bebekali954@gmail.com>
2025-02-08 16:54:24 +0300

    Update password-generator.py
    password generator

--- password-generator.py
+++ password-generator.py
@@ -9,3 +9,27 @@ for i in range(sifre_uzunlugu):
     sifre += random.choice(rastgele)
 
 print(sifre)
+
+cevap = input("Tekrar denemek ister misiniz?")
+
+if cevap == "hayır":
+        print("Size yardım ettiğim için mutluyum.")
+else:
+    while True:
+            sifre_uzunlugu = int(input("Lütfen şifrenin uzunluğunu giriniz."))
+
+            sifre = "" 
+
+            for i in range(sifre_uzunlugu):
+                sifre += random.choice(rastgele)
+            print(sifre)
+            cevap = input("Tekrar denemek ister misiniz?")
+
+            if cevap == "hayır":
+                print("Size yardım ettiğim için mutluyum.")
+                break
+
+
+    
+
+
